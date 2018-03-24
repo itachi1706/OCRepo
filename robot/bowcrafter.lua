2fe444728b4d845d4f36595a091dcaa11af08eed
itachi1706 <kennethsohyq@gmail.com>
2018-03-24 22:32:27 +0800

    Some bug fixes and debug messages

--- robot/bowcrafter.lua
+++ robot/bowcrafter.lua
@@ -10,6 +10,7 @@ inv = component.inventory_controller
 local lowPowerVal = 50 -- Value for Low Power
 local chargerSlot = 16 -- Slot to put OpenComputers Charger into
 local chestSlot = 15 -- Slot to put chest into
+local debug = true -- Set to false in prod
 
 function getPowerPercent()
     local maxPower = computer.maxEnergy()
@@ -36,24 +37,30 @@ function detectPowerFull()
 end
 
 function recharge(chargerSlot)
+    print("Recharging robot...")
     robot.select(chargerSlot)
-    while robot.compare() == false do
+    while robot.compare(true) == false do
         robot.turnLeft()
     end
     rs.setOutput(sides.front, 15)
-    while detectPowerFull == false do
+    while detectPowerFull() == false do
         os.sleep(5)
     end
     rs.setOutput(sides.front, 0)
     robot.select(1)
+    print("Recharge complete")
 end
 
 function crafting()
     c.craft(1)
+    print("Crafting Complete")
 end
 
 function isBrokenBow(slot)
     item = inv.getStackInSlot(sides.front, slot)
+    if debug then
+        print(item.name.." | "..item.damage.." | "..item.durability.." | ("..slot..")")
+    end
     if item.name == "minecraft:bow" then -- Check if Bow
         if item.damage == item.maxDamage then -- Check durability
             return false
@@ -65,6 +72,9 @@ end
 
 function isBrokenBowInt(slot)
     item = inv.getStackInInternalSlot(slot)
+    if debug then
+        print(item.name.." | "..item.damage.." | "..item.durability.." | ("..slot..")")
+    end
     if item.name == "minecraft:bow" then -- Check if Bow
         if item.damage == item.maxDamage then -- Check durability
             return false
@@ -76,23 +86,28 @@ end
 
 function ensureCrafterClean()
     for i=1,12 do
+        robot.select(i)
         robot.drop()
     end
+    print("Cleaned crafting slots")
 end
 
 function getBrokenBowsToCraft()
     local size = inv.getInventorySize(sides.front)
     ensureCrafterClean()
     local first = false
+    robot.select(1)
     for i = 1, size do
         if isBrokenBow(i) == true then
-            inv.suckFromSlot(i) -- Get Bow
+            inv.suckFromSlot(sides.front, i) -- Get Bow
             if first == false then
                 first = true
+                robot.select(2)
             else
                 robot.select(1)
                 crafting() -- Craft together
                 if isBrokenBowInt(1) == false then
+                    print("Repaired item dropped back into chest")
                     robot.drop()
                     first = false
                 end
@@ -106,10 +121,12 @@ while true do
         recharge(chargerSlot)
     end
     robot.select(chestSlot)
-    while robot.compare() == false do
+    while robot.compare(true) == false do
         robot.turnLeft()
     end
+    print("Preparing to repair broken bows by merging them together")
     getBrokenBowsToCraft()
+    print("Completed checking inventory for broken bows. Resting for 5 minutes")
     os.sleep(300)
 end
 
